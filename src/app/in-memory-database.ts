4a5945d773b35d28a352bc50e9c4c89c36324302
janaina.vasco-at-353667516549 <janaina.vasco@nibspe.com.br>
2019-08-06 12:31:49 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model'
+
 export class InMemoryDatabase implements InMemoryDbService {
   createDb(){
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'Pagamento de contas da casa' },
       { id: 2, name: 'Saúde', description: 'Plano de saúde e Remédios' },
       { id: 3, name: 'Lazer', description: 'Cinema, parques, praias, etc' },
