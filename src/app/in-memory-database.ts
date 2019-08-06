51f4a345999232b0038644dfd90c4ef6c3b97a05
janaina.vasco-at-353667516549 <janaina.vasco@nibspe.com.br>
2019-08-06 12:27:40 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb(){
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagamento de contas da casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de saúde e Remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praias, etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de Salário' },
+      { id: 5, name: 'Freelas', description: 'Trabalhos como Freelancer' },
+    ];
+    return { categories }
+  }
+}
